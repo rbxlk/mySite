25edd12747ff637ef7569375cf012c54a5222900
rbxlk <rbxlk@protonmail.com>
2020-06-16 13:46:34 +0100

    Update siteMetadata

--- mySite/gatsby-config.js
+++ mySite/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `rbxlk`,
+    description: `Welcome to my personal website created using React and Gatsby, I hope you enjoy your stay.`,
+    author: `@rbxlk`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
