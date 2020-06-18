30cf84b16d8d3cc7af6693269b04ebf6317bd6fd
rbxlk <rbxlk@protonmail.com>
2020-06-18 13:23:10 +0100

    Add a Container to the Index page
    Update sitemetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `rbxlk - Frontend Developer`,
+    title: `RbxLk - Frontend Developer`,
     description: `Welcome to my personal website created using React and Gatsby, I hope you enjoy your stay.`,
     author: `@rbxlk`,
   },
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,18 +4,22 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-
+import Container from "react-bootstrap/Container"
 const IndexPage = () => (
   <Layout>
-    {/* <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
+    <Container style={{
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <SEO title="Home" />
+      <h1>Welcome to my website!</h1>
+      <p>Currently under construction</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+        <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      </Container>
   </Layout>
 )
 
