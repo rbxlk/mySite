b5c3aaf38a1392b48df4fc0b3baacc411b1b040b
rbxlk <rbxlk@protonmail.com>
2020-06-18 13:21:33 +0100

    Update the header component to use react-bootstrap components and layout

--- src/components/header.js
+++ src/components/header.js
@@ -1,29 +1,35 @@
-import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import Container from "react-bootstrap/Container"
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
+import Button from "react-bootstrap/Button"
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
+import Form from "react-bootstrap/Form"
+import NavDropdown from "react-bootstrap/NavDropdown"
+import FormControl from "react-bootstrap/FormControl"
+import "./header.css"
 
 const Header = ({ siteTitle }) => (
-  <Container style={{
-    background: `#900C3F`,
-    padding: `1rem`
-  }}>
-    <Row>
-      <Col>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </Col>
-    </Row>
-  </Container>
+  <Navbar className="bg-custom" variant="dark" expand="lg" >
+    <Navbar.Brand href="/">{siteTitle}</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="#link">Link</Nav.Link>
+        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+      <Form inline>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button variant="success">Search</Button>
+      </Form>
+    </Navbar.Collapse>
+  </Navbar>
 )
 
 Header.propTypes = {
